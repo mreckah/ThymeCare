6d0cb2af86fd05a82c21c739a986da0c1c75d651
mreckah <m.reckahwalt@gmail.com>
2025-03-17 09:42:00 +0000

    Project setup

---
+++ src/main/java/com/tp/ThymeCare/ThymeCareApplication.java
@@ -0,0 +1,13 @@
+package com.tp.ThymeCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/ThymeCare/ThymeCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.ThymeCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
