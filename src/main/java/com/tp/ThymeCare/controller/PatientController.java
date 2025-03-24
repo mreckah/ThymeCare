c1c3b03f270673c3b6693aabd239aa17448b6d79
mreckah <m.reckahwalt@gmail.com>
2025-03-24 11:59:09 +0000

    Beg Second Part

--- src/main/java/com/tp/ThymeCare/controller/PatientController.java
+++ src/main/java/com/tp/ThymeCare/controller/PatientController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,6 +30,10 @@ public class  PatientController{
         model.addAttribute("currentPage",p);
         model.addAttribute("keyword",kw);
         return "patients";
-
+    }
+    @GetMapping("/delete")
+    public String delete(int id,String kw,int p){
+        repo.deleteById(id);
+        return "redirect:/index?page="+p+"&keyword="+kw;
     }
 }
